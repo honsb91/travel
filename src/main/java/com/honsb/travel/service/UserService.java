5ca3bfb482d1c658f5027496c5ab17ac71e742f8
honsb91 <114128759+honsb91@users.noreply.github.com>
2024-11-18 04:49:23 +0900

    .

--- src/main/java/com/honsb/travel/service/UserService.java
+++ src/main/java/com/honsb/travel/service/UserService.java
@@ -1,6 +1,8 @@
 package com.honsb.travel.service;
 
+import com.honsb.travel.domain.dto.UserDto;
 import com.honsb.travel.domain.dto.UserJoinRequest;
+import com.honsb.travel.domain.entity.User;
 import com.honsb.travel.repository.CommentRepository;
 import com.honsb.travel.repository.LikeRepository;
 import com.honsb.travel.repository.UserRepository;
@@ -9,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,8 +40,60 @@ public class UserService {
             bindingResult.addError(new FieldError("req","passwordCheck","비밀번호가 일치하지 않습니다."));
         }
 
+        if (req.getNickname().isEmpty()){
+            bindingResult.addError(new FieldError("req","nickname","닉네임이 비어있습니다."));
+        }else if (req.getNickname().length() > 10){
+            bindingResult.addError(new FieldError("req","nickname","닉네임이 10자가 넘습니다."));
+        }else if(userRepository.existsByNickname(req.getNickname())){
+            bindingResult.addError(new FieldError("req","nickname","닉네임이 중복됩니다."));
+        }
 
+        return bindingResult;
 
     }
+
+    public void join(UserJoinRequest req){
+        userRepository.save(req.toEntity(encoder.encode(req.getPassword())));
+    }
+
+    public User myInfo(String loginId){
+        return userRepository.findByLoginId(loginId).get();
+    }
+
+    public BindingResult editValid(UserDto dto,BindingResult bindingResult, String loginId){
+        User loginUser = userRepository.findByLoginId(loginId).get();
+
+        if(dto.getNowPassword().isEmpty()){
+            bindingResult.addError(new FieldError("dto","nowPassword","현재 비밀번호가 비어있습니다."));
+        }else if (!encoder.matches(dto.getNowPassword(), loginUser.getPassword())){
+            bindingResult.addError(new FieldError("dto","nowPassword","현재 비밀번호가 틀렸습니다."));
+        }
+
+        if(!dto.getNewPassword().equals(dto.getNewPasswordCheck())){
+            bindingResult.addError(new FieldError("dto","newPasswordCheck","비밀번호가 일치하지 않습니다."));
+        }
+
+        if (dto.getNickname().isEmpty()){
+            bindingResult.addError(new FieldError("dto","nickname","닉네임이 비어있습니다."));
+        }else if (dto.getNickname().length() > 10){
+            bindingResult.addError(new FieldError("dto","nickname","닉네임이 10자가 넘습니다."));
+        }else if (!dto.getNickname().equals(loginUser.getNickname()) && userRepository.existsByNickname(dto.getNickname())){
+            bindingResult.addError(new FieldError("dto","nickname","닉네임이 중복됩니다."));
+        }
+
+        return bindingResult;
+    }
+
+    @Transactional
+    public void edit(UserDto dto,String loginId){
+        User loginUser = userRepository.findByLoginId(loginId).get();
+
+        if (dto.getNewPassword().equals("")){
+            loginUser.edit(loginUser.getPassword(), dto.getNickname());
+        }else {
+            loginUser.edit(encoder.encode(dto.getNewPassword()), dto.getNickname());
+        }
+    }
+
 
 }
