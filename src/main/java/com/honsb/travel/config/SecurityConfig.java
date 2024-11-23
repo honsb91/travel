fc6820b3cc5e33f866ca41b7e32b94d5c205002a
honsb91 <114128759+honsb91@users.noreply.github.com>
2024-11-23 19:28:25 +0900

    .

--- src/main/java/com/honsb/travel/config/SecurityConfig.java
+++ src/main/java/com/honsb/travel/config/SecurityConfig.java
@@ -46,7 +46,20 @@ public class SecurityConfig {
 
                 // 폼 로그인
                 .formLogin()
-                .loginPage()
+                .loginPage("/users/login") //로그인 페이지
+                .usernameParameter("loginId") // 로그인에 사용될 id
+                .passwordParameter("password") // 로그인에 사용될 password
+                .failureUrl("/users/login?fail")// 로그인 실패 시 redirect 될 URL => 실패 메세지 출력
+                .successHandler(new MyLoginSuccessHandler(userRepository)) // 로그인 성공 시 실행
+                .and()
+
+                // 로그아웃
+                .logout()
+                .logoutUrl("/users/logout") // 로그아웃 URL
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(new MyLogoutSuccessHandler())
+                .and()
+                .build();
 
     }
 }
